f971e7a4ede11be7662fe57a1ad4168053a46ea8
Andrew Murdoch <amurdoch.cc@gmail.com>
2021-04-22 14:10:14 -0400

    Map changing code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {
   Component,
   OnInit,
-  OnDestroy,
+  OnDestroy, SimpleChanges,
 } from '@angular/core';
 
 @Component({
@@ -10,9 +10,31 @@ import {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  public page: string = 'dashboard';
+  public page = 'dashboard';
+  public timeout = 30000;
+  public stop = true;
 
   ngOnInit(): void {
+    this.stopToggle(true);
+  }
+
+  stopToggle(fromInit: boolean = false): void {
+    const buttons = ['dashboard', 'esri1', 'esri2', 'esri3', 'esri4', 'esri5'];
+    let index = 0;
+
+    if (fromInit === false) {
+      this.stop = !this.stop;
+    }
+
+    if (this.stop === true) {
+      const cycleInterval = setInterval(() => {
+        this.page = buttons[index];
+        index++;
+        if (index >= buttons.length) {
+          index = 0;
+        }
+      }, this.timeout);
+    }
   }
 
   ngOnDestroy(): void {
